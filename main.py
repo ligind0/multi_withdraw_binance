e6a1c511e4ea8388bb51130fa49d36ad6f0b2136
Ligindo <andreymaster2002@gmail.com>
2023-04-02 16:43:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -24,10 +24,11 @@ def add_rand_to_num(num):
     return str(round(random.uniform(num,1.15*num), 6))
 
 
-def withdraw_multiple(coin, addresses, amount, delay, network=None ):
+def withdraw_multiple(coin, addresses, amount, delay, rand, network=None ):
     addresses_list = [address.strip() for address in addresses.split('\n') if address.strip()]
     for address in addresses_list:
-        amount = add_rand_to_num(float(amount))
+        if rand:
+            amount = add_rand_to_num(float(amount))
         withdraw(coin, address, amount, network=network)
         print(f"{coin}, {address}, {amount}, {network}")
         print(f"Sleeping {delay} seconds")
@@ -39,17 +40,22 @@ def withdraw_gui():
         address = address_entry.get('1.0', 'end-1c')
         amount = amount_entry.get()
         network = network_var.get()
-        delay = int(delay_entry.get())
+        delay = delay_entry.get()
+        if delay == '': delay = 0
+        rand = rand_var.get()
         
         if address:
-            withdraw_multiple(coin, address, amount, delay,network=network )
+            withdraw_multiple(coin, address, amount, int(delay), rand, network=network)
         else:
             withdraw(coin, address, amount, network=network)
 
     root = tk.Tk()
     root.title('Binance Withdraw')
-    root.geometry('400x300')
+    root.geometry('420x300')
 
+    rand_var = tk.BooleanVar()
+    rand_check_box = tk.Checkbutton(root, text='Rand 15%',variable=rand_var, onvalue=True, offvalue=False)
+    rand_check_box.grid(row=5, column=2, padx=0, pady=20)
     coin_label = ttk.Label(root, text='Coin:')
     coin_label.grid(row=0, column=0, padx=5, pady=5)
     assets = client.get_withdraw_history()
@@ -57,15 +63,15 @@ def withdraw_gui():
     coin_entry = ttk.Combobox(root, values=assets_list, width=30)
     coin_entry.grid(row=0, column=1, padx=5, pady=5)
 
-    address_label = ttk.Label(root, text='Address:')
+    address_label = ttk.Label(root, text='Addresses:')
     address_label.grid(row=1, column=0, padx=5, pady=5)
     address_entry = tk.Text(root, height=4, width=30)
     address_entry.grid(row=1, column=1, padx=5, pady=5)
 
-    delay_label = ttk.Label(root, text='Задержка:')
-    delay_label.grid(row=5, column=0, padx=5, pady=5)
+    delay_label = ttk.Label(root, text='Delay:')
+    delay_label.grid(row=4, column=0, padx=5, pady=5)
     delay_entry = tk.Entry(root, width=30)
-    delay_entry.grid(row=5, column=1, padx=5, pady=5)
+    delay_entry.grid(row=4, column=1, padx=5, pady=5)
 
     amount_label = ttk.Label(root, text='Amount:')
     amount_label.grid(row=2, column=0, padx=5, pady=5)
@@ -79,10 +85,10 @@ def withdraw_gui():
     network_optionmenu.grid(row=3, column=1, padx=5, pady=5)
 
     withdraw_button = ttk.Button(root, text='Withdraw', command=do_withdraw)
-    withdraw_button.grid(row=4, column=1, padx=10, pady=20)
+    withdraw_button.grid(row=5, column=1, padx=5, pady=20)
 
     clear_button = ttk.Button(root, text='Clear', command=lambda: address_entry.delete('1.0', tk.END))
-    clear_button.grid(row=4, column=0, padx=10, pady=5)
+    clear_button.grid(row=5, column=0, padx=10, pady=5)
 
     root.mainloop()
 
